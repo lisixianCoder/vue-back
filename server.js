dd6c3e98a89b2ff4aee562308e5ab564fa9967ad
lisixianCoder <357047750@qq,com>
2019-08-24 17:07:05 +0800

    msg

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const httpServer = require('http')
-const userObj = [
+let httpServer = require('http')
+let userObj = [
     {'name':'lsx','age':18}
     {'name':'zhufeng','age':20}
 ]
-let server =  httpServer.createServer((req,res)=>{
+let server =  httpServer.createServer(function(req,res){
     res.setHeader('Access-Contrl-Allow-Origin','*')
-    if(req.url==='/api/getUser') {
+    if(req.url==='/api/users') {
         res.end(JSON.stringify(userObj))
     }else{
         res.end('URL NOT FOUND')
     }
 })
-httpServer.listen(3000,()=>{
+server.listen(3000,function(){
     console.log('3000端口服务已经启动')
 })
